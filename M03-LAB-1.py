1f3546e54e4c4b4724300b3b82b4ab31aff0472a
kclem028 <kclemons28@ivytech.edu>
2024-11-12 23:11:05 -0500

    Add files via upload

--- M03-LAB-1.py
+++ M03-LAB-1.py
@@ -1,3 +1,6 @@
+#By Kyle Clemons
+
+#Creates superclass for vehicles
 class Vehicle:
     def __init__(self):
         pass
@@ -9,6 +12,7 @@ class automobile(Vehicle):
         self.doors = doors
         self.roof = roof
 
+#Asks for attributes for the Vehicle sub-class, test code
 #automobile1 = automobile("2024", "Dodge", "Hellcat", "4", "Solid")
 
 year = int(input("What is the model year of your car? "))
@@ -17,6 +21,7 @@ model = input("What is the model of your car? ")
 doors = int(input("How many doors are on your car? "))
 roof = input("Is your car a solid-top or a convertible? ")
 
+#Extra error correction code. Unused.
 #Checks for a valid vehicle year.
 #if year != int:
 #   print("Please type a number for the vehicle's model year")
@@ -35,17 +40,18 @@ roof = input("Is your car a solid-top or a convertible? ")
 #    print("Invalid door types.")
 #    input("How many doors do you have? ")
 
+#Will print out the input for the Vehicle subclass
 print(automobile,year,make,model,doors,roof)
 
-
+#Individual inputs for each Vehicle subclass input
 #automobile2 = int(input(automobile.year()))
-automobile2 = input(automobile.make())
-automobile2 = input(automobile.model())
-automobile2 = input(automobile.doors())
-automobile2 = input(automobile.roof())
+#automobile2 = input(automobile.make())
+#automobile2 = input(automobile.model())
+#automobile2 = input(automobile.doors())
+#automobile2 = input(automobile.roof())
 
-print(automobile2.year)
-print(automobile2.make)
-print(automobile2.model)
-print(automobile2.doors)
-print(automobile2.roof)
+#print(automobile2.year)
+#print(automobile2.make)
+#print(automobile2.model)
+#print(automobile2.doors)
+#print(automobile2.roof)
